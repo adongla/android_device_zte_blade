c618a62442a5a100d155c25c7fe32124193784e5
Lalit Maganti <tilal6991@gmail.com>
2012-02-25 20:51:02 +0000

    Change directory structure

--- device_blade.mk
+++ device_blade.mk
@@ -30,41 +30,41 @@ PRODUCT_PACKAGES += \
     sensors.blade
 
 PRODUCT_COPY_FILES += \
-    device/zte/blade/prebuilt/system/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/zte/blade/prebuilt/system/usr/keylayout/blade_keypad.kl:system/usr/keylayout/blade_keypad.kl \
-    device/zte/blade/prebuilt/system/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
+    device/zte/blade/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/zte/blade/prebuilt/usr/keylayout/blade_keypad.kl:system/usr/keylayout/blade_keypad.kl \
+    device/zte/blade/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
 
 # fstab
 PRODUCT_COPY_FILES += \
-    device/zte/blade/prebuilt/system/etc/vold.fstab:system/etc/vold.fstab
+    device/zte/blade/prebuilt/etc/vold.fstab:system/etc/vold.fstab
 
 # Init
 PRODUCT_COPY_FILES += \
-    device/zte/blade/prebuilt/root/init.blade.rc:root/init.blade.rc \
-    device/zte/blade/prebuilt/root/init.blade.usb.rc:root/init.blade.usb.rc \
-    device/zte/blade/prebuilt/root/ueventd.blade.rc:root/ueventd.blade.rc
+    device/zte/blade/prebuilt/init.blade.rc:root/init.blade.rc \
+    device/zte/blade/prebuilt/init.blade.usb.rc:root/init.blade.usb.rc \
+    device/zte/blade/prebuilt/ueventd.blade.rc:root/ueventd.blade.rc
 
 # Audio
 PRODUCT_COPY_FILES += \
-    device/zte/blade/prebuilt/system/lib/libcamera.so:system/lib/libcamera.so \
-    device/zte/blade/prebuilt/system/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    device/zte/blade/prebuilt/lib/libcamera.so:system/lib/libcamera.so \
+    device/zte/blade/prebuilt/lib/libcameraservice.so:system/lib/libcameraservice.so \
     device/zte/blade/prebuilt/system/lib/libcamera_client.so:system/lib/libcamera_client.so \
-    device/zte/blade/prebuilt/system/lib/libril.so:system/lib/libril.so \
-    device/zte/blade/prebuilt/system/lib/libcamera.so:obj/lib/libcamera.so \
-    device/zte/blade/prebuilt/system/lib/libcameraservice.so:obj/lib/libcameraservice.so \
-    device/zte/blade/prebuilt/system/lib/libcamera_client.so:obj/lib/libcamera_client.so \
-    device/zte/blade/prebuilt/system/lib/libril.so:obj/lib/libril.so \
-    device/zte/blade/prebuilt/system/lib/hw/gralloc.blade.so:system/lib/hw/gralloc.blade.so \
-    device/zte/blade/prebuilt/system/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
-    device/zte/blade/prebuilt/system/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
-    device/zte/blade/prebuilt/system/lib/hw/copybit.blade.so:system/lib/hw/copybit.blade.so
+    device/zte/blade/prebuilt/lib/libril.so:system/lib/libril.so \
+    device/zte/blade/prebuilt/lib/libcamera.so:obj/lib/libcamera.so \
+    device/zte/blade/prebuilt/lib/libcameraservice.so:obj/lib/libcameraservice.so \
+    device/zte/blade/prebuilt/lib/libcamera_client.so:obj/lib/libcamera_client.so \
+    device/zte/blade/prebuilt/lib/libril.so:obj/lib/libril.so \
+    device/zte/blade/prebuilt/lib/hw/gralloc.blade.so:system/lib/hw/gralloc.blade.so \
+    device/zte/blade/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/zte/blade/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    device/zte/blade/prebuilt/lib/hw/copybit.blade.so:system/lib/hw/copybit.blade.so
 #    device/zte/blade/prebuilt/system/lib/hw/audio.primary.blade.so:system/lib/hw/audio.primary.blade.so \
 
 # WLAN + BT
 PRODUCT_COPY_FILES += \
-    device/zte/blade/prebuilt/system/etc/init.bt.sh:system/etc/init.bt.sh \
-    device/zte/blade/prebuilt/system/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-    device/zte/blade/prebuilt/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    device/zte/blade/prebuilt/etc/init.bt.sh:system/etc/init.bt.sh \
+    device/zte/blade/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/zte/blade/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 # Install the features available on this device.
 PRODUCT_COPY_FILES += \
@@ -72,14 +72,14 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
 
 # Kernel Modules
-PRODUCT_COPY_FILES += device/zte/blade/prebuilt/system/wifi/ar6000.ko:system/wifi/ar6000.ko 
+PRODUCT_COPY_FILES += device/zte/blade/prebuilt/wifi/ar6000.ko:system/wifi/ar6000.ko 
 
 # WiFi firmware
 PRODUCT_COPY_FILES += \
-    device/zte/blade/prebuilt/system/wifi/regcode:system/wifi/regcode \
-    device/zte/blade/prebuilt/system/wifi/data.patch.hw2_0.bin:system/wifi/data.patch.hw2_0.bin \
-    device/zte/blade/prebuilt/system/wifi/athwlan.bin.z77:system/wifi/athwlan.bin.z77 \
-    device/zte/blade/prebuilt/system/wifi/athtcmd_ram.bin:system/wifi/athtcmd_ram.bin
+    device/zte/blade/prebuilt/wifi/regcode:system/wifi/regcode \
+    device/zte/blade/prebuilt/wifi/data.patch.hw2_0.bin:system/wifi/data.patch.hw2_0.bin \
+    device/zte/blade/prebuilt/wifi/athwlan.bin.z77:system/wifi/athwlan.bin.z77 \
+    device/zte/blade/prebuilt/wifi/athtcmd_ram.bin:system/wifi/athtcmd_ram.bin
 
 # Media profile
 PRODUCT_COPY_FILES += \
