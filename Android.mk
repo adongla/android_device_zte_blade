5f1094166c77ac7dd1f367937b151d0656adad2d
Lalit Maganti <tilal6991@gmail.com>
2012-03-04 20:17:50 +0000

    Cleanup

--- Android.mk
+++ Android.mk
@@ -13,7 +13,5 @@
 # limitations under the License.
 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),blade)
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(call all-makefiles-under,device/zte/blade)
 endif
